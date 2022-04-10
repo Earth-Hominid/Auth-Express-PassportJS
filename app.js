1ee97127320d045f650a8a4de2eea1a87d28f486
C <earthhominid@gmail.com>
2022-04-10 16:10:37 -0300

    commiting majority of files. Authentication logic working, passportJS working, routes working

---
+++ app.js
@@ -0,0 +1,99 @@
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcryptjs = require('bcryptjs');
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const colors = require('colors');
+const connectDB = require('./config/db');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+
+const User = mongoose.model(
+  'User',
+  new Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+  })
+);
+
+app.set('views', __dirname);
+app.set('view engine', 'ejs');
+
+app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
+
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username }, (err, user) => {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username' });
+      }
+      if (user.password !== password) {
+        return done(null, false, { message: 'Incorrect password' });
+      }
+      return done(null, user);
+    });
+  })
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+// Middleware functions are simply functions that take the req and res objects, manipulate them, and pass them on through the rest of the app.
+// If you insert this code somewhere between where you instantiate the passport middleware and before you render your views, you will have access to the currentUser variable in all of your views, and you won’t have to manually pass it into all of the controllers in which you need it.
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
+
+app.get('/', (req, res) => res.render('index', { user: req.user }));
+app.get('/sign-up', (req, res) => res.render('sign-up-form'));
+
+app.post('/sign-up', (req, res, next) => {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
+  }).save((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('/');
+  });
+});
+
+app.post(
+  '/log-in',
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/',
+  })
+);
+
+app.get('/log-out', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
+app.listen(port, () => console.log(`app listening on port ${port}`));
